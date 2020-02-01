b2df14e0e695fb1b7364106d327bcebfa9476634
Siniorss <s.saadat@student.sharif.edu>
2020-02-01 19:37:15 +0330

    final commit

--- Code/Code.ino
+++ Code/Code.ino
@@ -26,6 +26,18 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
+
+char keys1[4] = {'.' , '!' , '?' , '1'};
+char keys2[4] = {'A' , 'B' , 'C' , '2'};
+char keys3[4] = {'D' , 'E' , 'F' , '3'};
+char keys4[4] = {'G' , 'H' , 'I'  , '4'};
+char keys5[4] = {'J' , 'K' , 'L' , '5'};
+char keys6[4] = {'M' , 'N' , 'O'  , '6' };
+char keys7[5] = {'P' , 'Q' , 'R' , 'S' , '7'};
+char keys8[4] = {'T' , 'U' , 'V' , '8'};
+char keys9[5] = {'W' , 'X' , 'Y' , 'Z' , '9'};
+char keys0[2] = {' ' , '0'};
+
 byte colPins[COLS] = {5 , 4 , 3 ,2}; //connect to the row pinouts of the keypad
 byte rowPins[ROWS] = {9 , 8 , 7 , 6}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
@@ -40,6 +52,10 @@ char msg[100] ;
 int ptr= 0 ;
 char package[100] ; 
 char delivery[] = "Delivered\0";
+unsigned long lastPress ;
+int pressCount = 1 ;
+char lastKey = '\0' ;
+bool caps = false ;
 
 char screen[100] ;
 int screenptr = 0 ;
@@ -55,15 +71,6 @@ char deliveryText[] = "Delivery" ;
 
 
 
-void setup() {
-  Serial.begin(9600);
-  lcd.begin(84, 48);
-  clearScreen() ;
-  radio.begin() ;
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setChannel(115);
-  radio.setDataRate(RF24_250KBPS);
-}
 
 
 
@@ -92,6 +99,18 @@ void clearScreen(){
   }
   screenptr = 0 ;
   lcd.clear();
+}
+
+void showWelcomePage(){
+  clearScreen();
+  lcd.setCursor(15 , 0);
+  lcd.print("Welcome");
+  lcd.setCursor(0,  1);
+  lcd.print(" * to set Address");
+  lcd.setCursor(0 , 2);
+  lcd.print(" # for Advanced settings");
+  lcd.setCursor(0 , 3);
+  lcd.print(" A to send a message");
 }
 
 
@@ -137,9 +156,12 @@ void send(){
           Serial.println(package); 
           if (package[0]==delivery[0]){
             undeliveredMSG = false ;
-            char success[] = "Sent" ;
+            char success[] = "Sent          Press any key to continue!" ;
             clearScreen() ;
             show(success);
+            char key2 ;
+            do key2 = keypad.getKey(); while(!key2);
+            showWelcomePage();
             break ;
           }else {
             Serial.println("got something from the receiver but it wasn't delivery !");
@@ -151,10 +173,13 @@ void send(){
        }
      }
      if (undeliveredMSG){
-      char failed[] = "Failed to send the message";
+      char failed[] = "Failed to send the message           Press any key to continue!";
       clearScreen() ;
       show (failed) ;
       undeliveredMSG = false ;
+      char key2 ;
+      do key2 = keypad.getKey(); while(!key2);
+      showWelcomePage();
      }
      deleteMessage() ;
 }
@@ -168,6 +193,22 @@ void deleteMessage(){
 }
 
 
+////////////////////////////////////////////////////////////////////////////////////// setup //////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(84, 48);
+  clearScreen() ;
+  radio.begin() ;
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(115);
+  radio.setDataRate(RF24_250KBPS);
+  showWelcomePage();
+  lastPress = millis();
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////// LOOP //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -179,24 +220,20 @@ void loop() {
   char key = keypad.getKey();
   if (key){
 
-
+  
 //////////////////////////////////////////////////////////////////////////////////stat changer//////////////////////////////////////////////////////////////////////////////////
     
     if (key == statChanger && !undeliveredMSG) {   //make sender
-      Serial.println();
-      Serial.println("ok what's your message ? ");
       clearScreen() ;
-      char string5[] = "what's the message ?\n";
+      char string5[] = "Start typing      \n";
       show(string5);
       lcd.setCursor(0, 1);
       if (addressSet){
         sender = true ;
-       // radio.stopListening() ;
         return ;
       }else{
         char err[] ="set the Address first" ;
         show(err) ;
-        
         return ;
       }
     }
@@ -205,7 +242,7 @@ void loop() {
 ///////////////////////////////////////////////////////////////////////////////set address//////////////////////////////////////////////////////////////////////////////////
 
     
-    else if (key == '*'){     
+    else if (key == '*' && !sender){     
       clearScreen() ;
       char string1[] = "setting address : \n" ;
       show(string1);
@@ -232,38 +269,171 @@ void loop() {
       char spaces[] = "      " ;
       show(spaces);
       show(thead) ;
-      char string4[] = "          press A to write a message" ;
+      char string4[] = "       press any key to continue" ;
       show(string4);
+      do key2[0] = keypad.getKey(); while(!key2[0]);
+      showWelcomePage();
       return ;  
+    }
+
+//////////////////////////////////////////////////////////////////////////////////channel & transfer rate///////////////////////////////////////////////////////////////////
+
+    else if (key == '#' && !sender){
+      
+      uint8_t ch = 0 ;
+      char chs[4] ;
+      chs[3] = '\0';
+      char key2[] = {'\0','\0'} ;
+      char hashtag[] = "Set channel:        " ;
+      while(true){
+        clearScreen();
+        show(hashtag);        
+        for (int i = 0 ; i < 3 ; i++){
+          key2[0] = keypad.getKey();
+          while(!key2[0] || key2[0] - '0' < 0 || key2[0] - '0' > 9){
+            key2[0] = keypad.getKey() ;
+          }
+          show(key2);
+          ch = ch * 10 + (key2[0]-'0');
+          chs[i] = key2[0] ;
+        }
+        if (ch < 126) break ;
+        else ch = 0 ;
+      }
+      clearScreen();
+      show("Channel is now : ");
+      show(chs);
+      radio.setChannel(chs);
+      show("       press any key to continue");
+      
+      do key2[0] = keypad.getKey(); while(!key2[0]);
+      
+      clearScreen();
+      lcd.print("Now Data rate : ");
+      lcd.setCursor(0,2);
+      lcd.print("1  :  1MBPS");
+      lcd.setCursor(0,3);
+      lcd.print("2  :  2MBPS");
+      lcd.setCursor(0,4);
+      lcd.print("3  :  250KBPS");
+      key2[0] = '\0';
+      while(!key2[0] || key2[0]-'0' < 1 || key2[0]-'0' > 3) {
+        key2[0] = keypad.getKey() ;
+      }
+      if (key2[0] == '1') radio.setDataRate(RF24_1MBPS);
+      else if (key2[0] == '2') radio.setDataRate(RF24_2MBPS);
+      else if (key2[0] == '3') radio.setDataRate(RF24_250KBPS);
+      clearScreen();
+      show("Data transfer has been set !          press any key to continue");
+      do key2[0] = keypad.getKey(); while(!key2[0]);
+      showWelcomePage();
+      
     }
 
 
 //////////////////////////////////////////////////////////////////////////////////sender//////////////////////////////////////////////////////////////////////////////////
 
     else if (sender){
+      
       if (key == 'C'){    //clear 1 character
         if (ptr > 0){
           ptr--;
           msg[ptr] = '\0';
           clearScreen() ;
           show(msg);
+          pressCount =  0;
         }
      }
      else if (key == 'B'){  //clear everything
         deleteMessage() ;
         clearScreen() ;
         show(msg);
+        pressCount =  0;
      }
      else if (key == 'D'){  //send
        send() ;
-      }
+       pressCount =  0;
+     }
+     else if (key == '#') caps = 1 - caps ;
+
+
+//////////////////////////////////////////////////////////////////////////////////TYPE//////////////////////////////////////////////////////////////////////////////////
+
+
+     
        else if (ptr < 100) { //type
-        msg[ptr] = key ;
-        ptr++ ;
-        clearScreen() ;
-        show(msg) ;
+        lastPress = millis() ;
+        while(millis() - lastPress <1000){
+          if (key && key){
+            if (key == '#') {
+              caps = 1 - caps ;
+              return  ;
+            }
+            if (lastKey == key){
+              pressCount++ ;
+              lastPress = millis() ;
+              ptr-- ;
+            }
+            else {
+              pressCount = 0 ;
+              lastKey = key ; 
+              lastPress = millis() ;
+            }
+
+            char ch ;
+            switch (lastKey) {
+              case '0' :
+                ch = keys0[pressCount % sizeof(keys0)] ;
+                break ;
+              case '1' :
+                ch = keys1[pressCount % sizeof(keys1)] ;
+                break ;
+              case '2' :
+                ch = keys2[pressCount % sizeof(keys2)] ;
+                break ;
+              case '3' :
+                ch = keys3[pressCount % sizeof(keys3)] ;
+                break ;
+              case '4' :
+                ch = keys4[pressCount % sizeof(keys4)] ;
+                break ;
+              case '5' :
+                ch = keys5[pressCount % sizeof(keys5)] ;
+                break ;
+              case '6' :
+                ch = keys6[pressCount % sizeof(keys6)] ;
+                break ;
+              case '7' :
+                ch = keys7[pressCount % sizeof(keys7)] ;
+                break ;
+              case '8' :
+                ch = keys8[pressCount % sizeof(keys8)] ;
+                break ;
+              case '9' :
+                ch = keys9[pressCount % sizeof(keys9)] ;
+                break ;
+              default : 
+                Serial.println("debug from case");
+                Serial.println(key);
+            }
+            if (!caps && lastKey != '0' && lastKey != '1' && lastKey != '#' && (ch - '0' > 9 || ch - '0' < 0) ) ch = ch - ('A' - 'a');
+              
+            msg[ptr] = ch ;
+            ptr++ ;
+            clearScreen();
+            show(msg) ;
+
+            
+          }
+          key = keypad.getKey() ;
+        }
         
+//        clearScreen() ;
+//        show(msg) ;
+        pressCount = 0 ;
+        lastKey = '\0';
       }
+      lastPress = millis();
     }
   }
 
@@ -281,10 +451,14 @@ void loop() {
           clearScreen() ;
           show(notif);
           show(package);
+          show("           press any key to continue!");
           radio.stopListening() ;
           radio.openWritingPipe(Address);
           radio.write(&delivery, sizeof(delivery));
           //undeliveredMSG=false;
+          char key2 ;
+          do key2 = keypad.getKey(); while(!key2);
+          showWelcomePage();
         }
     }
     
